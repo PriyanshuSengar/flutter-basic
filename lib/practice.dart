28a429fc5355b2d944069bfc6e3758417d94c42c
PriyanshuSenger <priyanshusengarps@gmail.com>
2025-03-25 22:44:25 +0530

    DataTypes_Variables_Functions

---
+++ lib/practice.dart
@@ -0,0 +1,57 @@
+// import 'dart:io';
+void main(){
+  print("Welcome to Dart");
+  // stdout.write("Enter Your Name :");
+  // var name = stdin.readLineSync();
+  // print("Welcome , $name");
+//  Human();
+// //  Declaration of Variable 
+//  int ? number;
+//  BigInt second_number;
+//  second_number =BigInt.parse("99973758069997375868746");
+//   print(second_number);
+//   num third_num=45.4;
+//   print(third_num);
+//   var fourth_val="Hello";
+//   fourth_val="World";
+//   dynamic fifth_val;
+//   fifth_val="Lukcey";
+//   fifth_val=50; 
+//   fifth_val=true;
+// var fucCalling =myClass();
+// fucCalling.myFunct("flutter");
+// fucCalling.myFunct("Dart");
+// fucCalling.addi(5,6);
+var list = [10,20,30,40];
+
+list.add(50);
+// print(list);
+var blank_list=[];
+// blank_list.addAll(list);
+// blank_list.add(60);
+blank_list.add("Hello");
+blank_list[0]="Luckey";
+blank_list.add("Hello World");
+// blank_list.insert(2,"Hello");
+list.replaceRange(0, 2, [1,2,3]);
+blank_list.insert(1, list);
+blank_list.remove("Luckey");
+blank_list.removeAt(1);
+
+print(blank_list);
+}
+// class myClass{
+//   myClass(){
+//     print("myClass object Created");
+//   }
+//   void myFunct(String name){
+//     print(name);
+//   }
+//   int addi(int a,int b){
+    
+//    int sum=a+b;
+//    print(sum);
+//    return sum;
+
+//   }
+// }
