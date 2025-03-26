7b998e5ec2962a81fa9c0d1078d7972c9b4ad5fc
PriyanshuSenger <priyanshusengarps@gmail.com>
2025-03-26 23:19:55 +0530

    Final_and_Const

--- lib/practice.dart
+++ lib/practice.dart
@@ -22,23 +22,35 @@ void main(){
 // fucCalling.myFunct("flutter");
 // fucCalling.myFunct("Dart");
 // fucCalling.addi(5,6);
-var list = [10,20,30,40];
+// var list = [10,20,30,40];
 
-list.add(50);
+// list.add(50);
 // print(list);
-var blank_list=[];
+// var blank_list=[];
 // blank_list.addAll(list);
 // blank_list.add(60);
-blank_list.add("Hello");
-blank_list[0]="Luckey";
-blank_list.add("Hello World");
-// blank_list.insert(2,"Hello");
-list.replaceRange(0, 2, [1,2,3]);
-blank_list.insert(1, list);
-blank_list.remove("Luckey");
-blank_list.removeAt(1);
+// blank_list.add("Hello");
+// blank_list[0]="Luckey";
+// blank_list.add("Hello World");
+// // blank_list.insert(2,"Hello");
+// list.replaceRange(0, 2, [1,2,3]);
+// blank_list.insert(1, list);
+// blank_list.remove("Luckey");
+// blank_list.removeAt(1);
 
-print(blank_list);
+// print(blank_list);
+// const list=[
+//   'Lukcey',
+//   'Naveen',
+//   'Ankit',
+//   'Nicku'
+// ];
+// list.add("Thakur");
+// print(list);
+// list=[
+
+// ]
+
 }
 // class myClass{
 //   myClass(){
