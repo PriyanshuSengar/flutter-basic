af97370bd903a511f82eae05ca31a3c304befb98
PriyanshuSenger <priyanshusengarps@gmail.com>
2025-03-28 22:37:40 +0530

    Container

--- lib/map_and_hasmap.dart
+++ lib/map_and_hasmap.dart
@@ -1,22 +1,22 @@
-main(){
-var map_name = {
-'Key1':'Value1',
-'Key2':2,
-'Key3':2.5,
-'Key4':true,
-};
-map_name['Key1']="Hello";
-print(map_name);
-print(map_name['Key1']);
-var employee_details = Map();
-employee_details['Key1']='Hello';
-print(employee_details);
-print(map_name.length);
-print(map_name.isEmpty);
-print(map_name.isNotEmpty);
-print(map_name.values);
-print(map_name.keys);
-print(map_name.remove('Key3'));
-print(map_name.containsKey('Key1'));
+// main(){
+// var map_name = {
+// 'Key1':'Value1',
+// 'Key2':2,
+// 'Key3':2.5,
+// 'Key4':true,
+// };
+// map_name['Key1']="Hello";
+// print(map_name);
+// print(map_name['Key1']);
+// var employee_details = Map();
+// employee_details['Key1']='Hello';
+// print(employee_details);
+// print(map_name.length);
+// print(map_name.isEmpty);
+// print(map_name.isNotEmpty);
+// print(map_name.values);
+// print(map_name.keys);
+// print(map_name.remove('Key3'));
+// print(map_name.containsKey('Key1'));
 
-}
+// }
