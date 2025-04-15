cf39fcb3d786e546c3d5743cff761624fded732f
PriyanshuSenger <priyanshusengarps@gmail.com>
2025-04-15 22:48:34 +0530

    hello world

---
+++ lib/ListView.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var arrNames = ["One", "Two", "Three", "Four", "Five"];
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text('Flutter Basics', style: TextStyle(fontSize: 25)),
+      ),
+
+      body: ListView.separated(
+        itemBuilder: (context, index) {
+          return Row(
+            children: [
+              
+              Text(
+                arrNames[index],
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+              ),
+              Text(
+                arrNames[index],
+                style: TextStyle(fontSize: 10, fontWeight: FontWeight.w400),
+              ),
+              Text(
+                arrNames[index],
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+              ),
+            ],
+          );
+        },
+        itemCount: arrNames.length,
+        // reverse: true,
+        // itemExtent: 200,
+        // scrollDirection: Axis.horizontal,
+        separatorBuilder: (context, index) {
+          return Divider(height: 200, thickness: 5);
+        },
+      ),
+      // Center(
+      //   child: ListView(
+      //     scrollDirection: Axis.horizontal,
+      //     children: [
+      //       Padding(
+      //         padding: const EdgeInsets.all(8.0),
+      //         child: Text(
+      //           "One",
+      //           style: TextStyle(fontSize: 21, fontWeight: FontWeight.w400),
+      //         ),
+      //       ),
+      //       Padding(
+      //         padding: const EdgeInsets.all(8.0),
+      //         child: Text(
+      //           "Two",
+      //           style: TextStyle(fontSize: 21, fontWeight: FontWeight.w400),
+      //         ),
+      //       ),
+      //       Padding(
+      //         padding: const EdgeInsets.all(8.0),
+      //         child: Text(
+      //           "Three",
+      //           style: TextStyle(fontSize: 21, fontWeight: FontWeight.w400),
+      //         ),
+      //       ),
+      //       Padding(
+      //         padding: const EdgeInsets.all(8.0),
+      //         child: Text(
+      //           "Four",
+      //           style: TextStyle(fontSize: 21, fontWeight: FontWeight.w400),
+      //         ),
+      //       ),
+      //       Padding(
+      //         padding: const EdgeInsets.all(8.0),
+      //         child: Text(
+      //           "Five",
+      //           style: TextStyle(fontSize: 21, fontWeight: FontWeight.w400),
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -37,65 +37,163 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
 
-        title: Text('Flutter Basics', style: TextStyle(fontSize: 25)),
+        title: Text('Flutter Basics', style: TextStyle(fontSize: 25,fontWeight: FontWeight.w200)),
       ),
-      body: Container(
-        // width: 300,
-        // height: 300,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          // mainAxisAlignment: MainAxisAlignment.spaceAround,
-          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          // mainAxisAlignment: MainAxisAlignment.center,
-          // mainAxisAlignment: MainAxisAlignment.end,
-          // crossAxisAlignment: CrossAxisAlignment.center,
-          // crossAxisAlignment: CrossAxisAlignment.center,
-          // crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              // crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        print("Button clicked");
-                      },
-                      child: Text("Click me"),
-                    ),
-                    ElevatedButton(
-                      onPressed: () => print("Just Pressed"),
-                      onLongPress: () {
-                        print("Button Long Pressed");
-                      },
-                      child: Text("Click me to"),
-                    ),
-                  ],
-                ),
-                Text("a", style: TextStyle(fontSize: 50)),
-                Text("b", style: TextStyle(fontSize: 20)),
-                Text("c", style: TextStyle(fontSize: 20)),
-                Text("d", style: TextStyle(fontSize: 20)),
-                Text("e", style: TextStyle(fontSize: 20)),
-              ],
-            ),
-            Text("a", style: TextStyle(fontSize: 50)),
-            Text("b", style: TextStyle(fontSize: 20)),
-            Text("c", style: TextStyle(fontSize: 20)),
-            Text("d", style: TextStyle(fontSize: 20)),
-            Text("e", style: TextStyle(fontSize: 20)),
-            ElevatedButton(
-              onPressed: () {
-                print("Clicked");
-              },
-              child: Text("Clicked"),
-            ),
-          ],
-        ),
-      ),
+
+      // body: Padding(
+      //   padding: const EdgeInsets.all(8.0),
+      //   child: SingleChildScrollView(
+      //     child: Column(
+      //       children: [
+      //         Padding(
+      //           padding: const EdgeInsets.all(5.0),
+      //           child: SingleChildScrollView(
+      //             scrollDirection: Axis.horizontal,
+      //             child: Row(
+      //               children: [
+      //                 Container(
+      //                   height: 200,
+      //                   width: 200,
+      //                   color: Colors.green,
+      //                   margin: EdgeInsets.only(right: 11),
+      //                 ),
+      //                 Container(
+      //                   height: 200,
+      //                   width: 200,
+      //                   color: Colors.black,
+      //                   margin: EdgeInsets.only(right: 11),
+      //                 ),
+      //                 Container(
+      //                   height: 200,
+      //                   width: 200,
+      //                   color: Colors.blue,
+      //                   margin: EdgeInsets.only(right: 11),
+      //                 ),
+      //                 Container(
+      //                   height: 200,
+      //                   width: 200,
+      //                   color: Colors.yellow,
+      //                   margin: EdgeInsets.only(right: 11),
+      //                 ),
+      //               ],
+      //             ),
+      //           ),
+      //         ),
+
+      //         Container(
+      //           height: 200,
+      //           // width: 200,
+      //           color: Colors.black,
+      //           margin: EdgeInsets.only(bottom: 11),
+      //         ),
+      //         Container(
+      //           height: 200,
+      //           // width: 200,
+      //           color: Colors.red,
+      //           margin: EdgeInsets.only(bottom: 11),
+      //         ),
+      //         Container(
+      //           height: 500,
+      //           width: 200,
+      //           color: Colors.yellow,
+      //           margin: EdgeInsets.only(bottom: 11),
+      //         ),
+      //         Container(child: Text("Luckey")),
+      //       ],
+      //     ),
+      //   ),
+      // ),
+
+      // //  Center(
+      // //   child: InkWell(
+      // //     child: Container(
+      // //       width: 200,
+      // //       height: 200,
+      // //       color: Colors.yellow,
+      // //       child: InkWell(
+      // //         onDoubleTap: () {
+      // //           print("Clicke here was pressed");
+      // //         },
+      // //         onTap: () {
+      // //           print("Click here was pressed once");
+      // //         },
+      // //         child: Center(
+      // //           child: Text(
+      // //             "Click here",
+      // //             style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700),
+      // //           ),
+      // //         ),
+      // //       ),
+      // //     ),
+
+      // //     onDoubleTap: () {
+      // //       print("Inwell Confirmed...");
+      // //     },
+      // //     onTap: () {
+      // //       print("Single tap Activated");
+      // //     },
+      // //     onLongPress: () {
+      //       print("Long Pressed");
+      //     },
+      //   ),
+      // ),
+      // Container(
+      //   // width: 300,
+      //   // height: 300,
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.start,
+      //     crossAxisAlignment: CrossAxisAlignment.start,
+      //     // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //     // mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //     // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //     // mainAxisAlignment: MainAxisAlignment.center,
+      //     // mainAxisAlignment: MainAxisAlignment.end,
+      //     // crossAxisAlignment: CrossAxisAlignment.center,
+      //     // crossAxisAlignment: CrossAxisAlignment.center,
+      //     // crossAxisAlignment: CrossAxisAlignment.center,
+      //     children: [
+      //       Row(
+      //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //         // crossAxisAlignment: CrossAxisAlignment.center,
+      //         children: [
+      //           Column(
+      //             children: [
+      //               ElevatedButton(
+      //                 onPressed: () {
+      //                   print("Button clicked");
+      //                 },
+      //                 child: Text("Click me"),
+      //               ),
+      //               ElevatedButton(
+      //                 onPressed: () => print("Just Pressed"),
+      //                 onLongPress: () {
+      //                   print("Button Long Pressed");
+      //                 },
+      //                 child: Text("Click me to"),
+      //               ),
+      //             ],
+      //           ),
+      //           Text("a", style: TextStyle(fontSize: 50)),
+      //           Text("b", style: TextStyle(fontSize: 20)),
+      //           Text("c", style: TextStyle(fontSize: 20)),
+      //           Text("d", style: TextStyle(fontSize: 20)),
+      //           Text("e", style: TextStyle(fontSize: 20)),
+      //         ],
+      //       ),
+      //       Text("a", style: TextStyle(fontSize: 50)),
+      //       Text("b", style: TextStyle(fontSize: 20)),
+      //       Text("c", style: TextStyle(fontSize: 20)),
+      //       Text("d", style: TextStyle(fontSize: 20)),
+      //       Text("e", style: TextStyle(fontSize: 20)),
+      //       ElevatedButton(
+      //         onPressed: () {
+      //           print("Clicked");
+      //         },
+      //         child: Text("Clicked"),
+      //       ),
+      //     ],
+      //   ),
+      // ),
       // Center(
       //   child: Container(
       //     child: Image.asset('assets/images/flutter2.jpg'),
