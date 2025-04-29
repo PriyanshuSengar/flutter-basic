846b72a1b1d2984eb25239587fc8e5a7d17ec318
PriyanshuSenger <priyanshusengarps@gmail.com>
2025-04-29 13:14:14 +0530

    Padding and Margin

---
+++ lib/MarginPadding.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Pages'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var arrNames = ["One", "Two", "Three", "Four", "Five"];
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text('Flutter Basics', style: TextStyle(fontSize: 25)),
+      ),
+
+      body: Container(
+        child: Text("Hello Flutter"),
+        color: Colors.amber[200],
+        margin: EdgeInsets.only(left: 20),
+        padding: EdgeInsets.only(top: 10, left: 4),
+        height: 50,
+        width: 90,
+      ),
+      // Padding(
+      //   padding: EdgeInsets.all(20),
+      //   child: Text('Flutter Developer', style: TextStyle(fontSize: 20)),
+      // ),
+    );
+  }
+}
