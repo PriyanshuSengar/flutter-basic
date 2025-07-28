7dc6868dd95b210db3811ab07c51f09e89cb7e71
PriyanshuSenger <priyanshusengarps@gmail.com>
2025-07-28 17:11:53 +0530

    styleandthemes

---
+++ lib/styleandthemes.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Journy',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        textTheme: TextTheme(
+          headlineSmall: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+          // subtitle1: TextStyle(
+          //     fontSize: 20,
+          //     fontWeight: FontWeight.bold,
+          //     fontStyle: FontStyle.italic,
+          //   ),
+        ),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Pages'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text('Flutter Learning', style: TextStyle(fontSize: 25)),
+      ),
+
+      body: Column(
+        children: [
+          Text(
+            "Luckey",
+            style: Theme.of(
+              context,
+            ).textTheme.headlineSmall!.copyWith(color: Colors.yellowAccent),
+          ),
+          Text(
+            "Luckey",
+            // style: Theme.of(context).textTheme.subtitle1,
+          ),
+          Text("Luckey", style: Theme.of(context).textTheme.headlineSmall),
+          Text(
+            "Luckey",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
