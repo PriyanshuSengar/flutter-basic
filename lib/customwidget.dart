be1af34a7e282dfc75ea937bd4cd067b6700457a
PriyanshuSenger <priyanshusengarps@gmail.com>
2025-08-22 16:18:18 +0530

    Custom Widget in flutter

---
+++ lib/customwidget.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Journy',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Pages'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text('Flutter Learning', style: TextStyle(fontSize: 25)),
+      ),
+
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Container(height: 200, color: Colors.green),
+            ),
+            Expanded(
+              flex: 4,
+              child: Container(height: 200, color: Colors.black),
+            ),
+            Expanded(
+              flex: 5,
+              child: Container(height: 200, color: Colors.lightBlueAccent),
+            ),
+            Expanded(flex: 2, child: Container(height: 200, color: Colors.red)),
+          ],
+        ),
+      ),
+    );
+  }
+}
