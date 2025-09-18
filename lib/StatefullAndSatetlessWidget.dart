4a1144717d74bad317178b027d7a8fe85d64f772
PriyanshuSenger <priyanshusengarps@gmail.com>
2025-09-18 21:09:54 +0530

    Statefull and Stateless Widget

---
+++ lib/StatefullAndSatetlessWidget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('stateful')),
+      body: Center(
+        child: Column(
+          children: [
+            Text('Counter'),
+            ElevatedButton(onPressed: () {}, child: Text('Increment Count')),
+          ],
+        ),
+      ),
+    );
+  }
+}
